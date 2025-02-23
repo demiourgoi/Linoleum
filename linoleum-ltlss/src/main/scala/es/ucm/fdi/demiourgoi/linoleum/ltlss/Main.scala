bc4eb3567bdd55fe9405ccecd65d6e5daf949da9
Juan Rodriguez <juan.rodriguez.hortala@gmail.com>
2025-02-23 21:13:22 +0100

    publish and query traces from Jaeger

--- linoleum-ltlss/src/main/scala/es/ucm/fdi/demiourgoi/linoleum/ltlss/Main.scala
+++ linoleum-ltlss/src/main/scala/es/ucm/fdi/demiourgoi/linoleum/ltlss/Main.scala
@@ -2,92 +2,58 @@ package es.ucm.fdi.demiourgoi.linoleum.ltlss
 
 import es.ucm.fdi.demiourgoi.sscheck.prop.tl.Formula._
 import org.specs2.matcher.MustMatchers._
-import io.grpc.{CallCredentials, Channel, Context, Grpc, Metadata, ManagedChannelBuilder, TlsChannelCredentials}
-import com.grafana.tempopb.{SearchRequest, StreamingQuerierGrpc, QuerierGrpc, TraceByIDRequest}
-// async because it is not StreamingQuerierBlockingStub
-// import com.grafana.tempopb.StreamingQuerierGrpc.StreamingQuerierStub
-import com.grafana.tempopb.StreamingQuerierGrpc.StreamingQuerierBlockingStub
+import com.google.protobuf.Timestamp
+import io.grpc.{Channel, ManagedChannelBuilder}
+import io.jaegertracing.api_v3.{QueryServiceGrpc, QueryServiceOuterClass}
 import java.time.{Duration, Instant}
-import java.util.concurrent.Executor
-import java.nio.file.Path
-import scala.io.Source
-import com.google.protobuf.ByteString
+import scala.jdk.CollectionConverters._
 
-object TempoAuthCredentials {
-    // FIXME
-    private val Token = {
-        val tokenPath = Path.of(System.getProperty("user.home"), ".otel", "grafana.creds")
-        val fileSrc = Source.fromFile(tokenPath.toString)
-        val token = fileSrc.getLines().mkString
-        fileSrc.close()
-        // println(s"Using token [$token]") // FIXME
-        token
-    }
-    val MetadataKey = Metadata.Key.of("Authorization", Metadata.ASCII_STRING_MARSHALLER)
-    val UserKey = Metadata.Key.of("User", Metadata.ASCII_STRING_MARSHALLER)
-    val ContextUsernameKey = Context.key("username")
-}
-class TempoAuthCredentials extends CallCredentials {
-    import TempoAuthCredentials._
-    override def applyRequestMetadata(requestInfo: CallCredentials.RequestInfo,
-                                      appExecutor: Executor,
-                                      metadataApplier: CallCredentials.MetadataApplier): Unit = {
-        // FIXME try catch https://github.dev/mark-cs/grpc-call-credentials/tree/master/src/main/java/uk/co/markcs/grpc
-        appExecutor.execute(() => {
-            val headers = new Metadata()
-            headers.put(MetadataKey, s"Basic $Token")
-           // headers.put(UserKey, "780056") // FIXME
-            metadataApplier(headers)
-        })
-    }
+object TimeUtils {
+    def instantToTimestamp(instant: Instant): Timestamp =
+        Timestamp.newBuilder()
+          .setSeconds(instant.getEpochSecond)
+          .setNanos(instant.getNano)
+          .build()
+
+    def timestampToInstant(timestamp: Timestamp): Instant =
+        Instant.ofEpochSecond(timestamp.getSeconds, timestamp.getNanos)
 }
 
 object Main extends App {
-    val host =  "tempo-prod-10-prod-eu-west-2.grafana.net" // "tempo-prod-10-prod-eu-west-2.grafana.net/tempo"
-    val credentials = new TempoAuthCredentials()
+    import TimeUtils._
+
+    val (host, port) = ("localhost", 16685)
     val channel: Channel = {
-        val builder = ManagedChannelBuilder.forAddress(host, 4317)
-        // builder.usePlaintext()
-        builder.useTransportSecurity()
+        val builder = ManagedChannelBuilder.forAddress(host, port)
+        builder.usePlaintext() // FIXME configurable, local jaeger is htt
         builder.build()
     }
-    /*
-    = Grpc.newChannelBuilderForAddress(
-        host, 443,
-        TlsChannelCredentials.create() // FIXME try with planin
-    ).build() */
-    val client = QuerierGrpc.newBlockingStub(channel)
-        // StreamingQuerierGrpc.newBlockingStub(channel)
+    val client = QueryServiceGrpc.newBlockingStub(channel)
     val now = Instant.now()
 
-    // returns an iterator because this is a server side streaming RPC
-    val searchRequest = SearchRequest.newBuilder()
-      // https://grafana.com/docs/tempo/latest/api_docs/#search unix epoch seconds
-      .setStart(math.round(now.minus(Duration.ofMinutes(10)).toEpochMilli / 1000))
-      .setEnd(math.round(now.toEpochMilli / 1000))
-      .setLimit(10)
-      .setSpansPerSpanSet(100)
-      // https://grafana.com/docs/tempo/latest/traceql/
-      .setQuery("{}")
-      .build()
-    println(s"Searching with query $searchRequest")
-    val traceID = "1bf45bf4e4c16cb8f96cfd25ff0db648"
-    val response = client.withCallCredentials(credentials).findTraceByID(
-        TraceByIDRequest.newBuilder()
-          .setTraceID(ByteString.copyFromUtf8(traceID))
-          .build()
-    )
-    println("got response")
-    println(response.getTrace)
-   /* val responses = client
-      .withCallCredentials(credentials)
-      .searchRecent(searchRequest)
-      // .search(searchRequest)
-    println(responses)
-    responses forEachRemaining { response =>
-      println(response)
+    // https://github.com/jaegertracing/jaeger-idl/blob/main/proto/api_v3/query_service.proto
+    val request = QueryServiceOuterClass.GetTraceRequest.newBuilder()
+      .setTraceId("adcd471c2419a12256c99086474f0e1b").build()
+    val tracesData = client.getTrace(request)
+    tracesData forEachRemaining  { response =>
+        println(response)
     }
-    */
+
+    val findTracesRequest = QueryServiceOuterClass.FindTracesRequest.newBuilder()
+      .setQuery(
+          QueryServiceOuterClass.TraceQueryParameters.newBuilder()
+            .setServiceName("linoleum.tools.simreplayer:kotlin")
+            .setStartTimeMin(
+               instantToTimestamp(now.minus(Duration.ofMinutes(10)))
+            )
+            .setStartTimeMax(instantToTimestamp(now))
+            .build()
+      ).build()
+    val searchResponse = client.findTraces(findTracesRequest).asScala.toList
+    println(s"Found ${searchResponse.size} traces")
+    searchResponse foreach { response =>
+        println(response)
+    }
 
     val formula = always{ x: Int => x must be_>(0) } during 10
     Console.println(s"Hello ${formula}")
