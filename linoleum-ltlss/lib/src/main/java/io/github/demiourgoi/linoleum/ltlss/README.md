dd7acd00b73115adc74d5134a4931c1c00fd8377
Juan Rodriguez Hortala <juanrh@pm.me>
2025-09-09 22:52:28 +0200

    Rename packages to io.github.demiourgoi

---
+++ linoleum-ltlss/lib/src/main/java/io/github/demiourgoi/linoleum/ltlss/README.md
@@ -0,0 +1,7 @@
+Generated from https://github.com/demiourgoi/jaeger-idl/tree/linoleum-ltlss-idl running:
+
+```bash
+make init-submodule
+make proto-all
+ls .proto-gen-polyglot/proto-gen-java/io/github/demiourgoi/linoleum/ltlss/
+``` 
---
+++ linoleum-ltlss/lib/src/main/scala/io/github/demiourgoi/linoleum/ltlss/sink/package.scala
@@ -0,0 +1,44 @@
+package io.github.demiourgoi.linoleum.ltlss
+
+import org.apache.flink.connector.mongodb.sink.MongoSink
+import org.apache.flink.streaming.api.datastream.DataStream
+import org.apache.flink.connector.base.DeliveryGuarantee
+import com.mongodb.client.model.InsertOneModel
+
+import org.slf4j.LoggerFactory
+
+import config.LinoleumConfig
+
+package object sink {
+  type EvaluatedTraceSink = MongoSink[EvaluatedTrace]
+}
+
+package sink {
+  object LinoleumSink {
+    private val log = LoggerFactory.getLogger(classOf[LinoleumSink])
+  }
+  class LinoleumSink(cfg: LinoleumConfig) {
+    import LinoleumSink._
+
+    def apply(evaluatedTraces: DataStream[EvaluatedTrace]): Unit = {
+      evaluatedTraces.sinkTo(buildSink())
+    }
+
+    private def buildSink(): MongoSink[EvaluatedTrace] = {
+      val mongoCfg = cfg.sink.mongoDb
+      MongoSink.builder[EvaluatedTrace]()
+        .setUri(mongoCfg.mongoUri)
+        .setDatabase(mongoCfg.mongoDatabase)
+        .setCollection(mongoCfg.mongoCollection)
+        .setBatchSize(mongoCfg.mongoBatchSize)
+        .setBatchIntervalMs(mongoCfg.mongoBatchIntervalMs)
+        .setMaxRetries(mongoCfg.mongoMaxRetries)
+        .setDeliveryGuarantee(DeliveryGuarantee.AT_LEAST_ONCE)
+        .setSerializationSchema((trace, _) => {
+          log.info("Writing evaluated trace {} to MongoDB", trace)
+          new InsertOneModel(trace.toBsonDocument)
+        })
+        .build()
+    }
+  }
+}
